5359788127d10327a6469d09e75620538aea5d97
Dax Buratto <daxburatto@gmail.com>
2021-01-15 12:08:10 -0600

    added error fixes and displaying repo issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container")
 var limitWarningEl = document.querySelector("#limit-warning")
+var repoNameEl = document.querySelector("#repo-name")
+
+
+var getRepoName = function () {  
+    // grab repo name from url query string
+    var queryString = document.location.search
+    var repoName = queryString.split("=")[1]
+    
+    if (repoName) {
+        // display repo name on the page
+        getRepoIssues(repoName)
+        repoNameEl.textContent = repoName
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html")
+    }
+}
 
 var getRepoIssues = function (repo) {  
     console.log(repo)
@@ -17,7 +34,7 @@ var getRepoIssues = function (repo) {
             })
         }
         else {
-            alert("There was a problem with your request!")
+            document.location.replace("./index.html")
         }
     })
 }
@@ -73,4 +90,4 @@ var displayWarning = function (repo) {
 
 
 // event listeners
-getRepoIssues("facebook/react")
+getRepoName()
