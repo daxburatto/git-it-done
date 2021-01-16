f5618a2530721a4fe1238519adef36b39d05b949
Dax Buratto <daxburatto@gmail.com>
2021-01-16 13:06:36 -0600

    added buttons for sorting projects

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form")
 var nameInputEl = document.querySelector("#username")
 var repoContainerEl = document.querySelector("#repos-container")
 var repoSearchTerm = document.querySelector("#repo-search-term")
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -20,6 +21,20 @@ var getUserRepos = function(user) {
         .catch(function (error) {  
             alert("Unable to connect to GitHub")
         })
+}
+
+var getFeaturedRepos = function (language) {  
+    var apiURL = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues"
+    
+    fetch(apiURL).then(function (response) {  
+        if (response.ok) {
+            response.json().then(function (data) {  
+                displayRepos(data.items, language)
+            })
+        } else {
+            alert("Error: " + response.statusText)
+        }
+    })
 }
 
 var formSubmitHandler = function (event) {  
@@ -84,6 +99,20 @@ var displayRepos = function (repos, searchTerm) {
     }
 }
 
+buttonClickHandler = function (event) {  
+    var language = event.target.getAttribute("data-language")
+    console.log(language)
+
+    if (language) {
+        getFeaturedRepos(language)
+
+        // clear old content
+        repoContainerEl.textContent = ""
+    }
+}
+
 // event listeners
 userFormEl.addEventListener("submit", formSubmitHandler)
+
+languageButtonsEl.addEventListener("click", buttonClickHandler)
 
